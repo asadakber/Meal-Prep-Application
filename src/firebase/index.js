b8a28685a834ac5c5909401fe0a76ff1136e62e3
asadakber <asad.akberweb@gmail.com>
2018-12-27 00:36:09 +0500

    first commit

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+   apiKey: "AIzaSyAvfagdi_7VNbb3sbopW-e9ZEQ5F_i1_Tc",
+    authDomain: "meal-prep-f619f.firebaseapp.com",
+    databaseURL: "https://meal-prep-f619f.firebaseio.com",
+    projectId: "meal-prep-f619f",
+    storageBucket: "meal-prep-f619f.appspot.com",
+    messagingSenderId: "155122577893"
+};
+firebase.initializeApp(config);
